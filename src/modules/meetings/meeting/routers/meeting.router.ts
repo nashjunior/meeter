16f9e587e45f2d7a6edc1ff00b83c1e9ae11ce82
nashjunior <nashjuniorjj@gmail.com>
2022-09-22 20:29:34 -0300

    feat: :truck: should register meetings controller from subdomain meeting on router

--- src/modules/meetings/meeting/routers/meeting.router.ts
+++ src/modules/meetings/meeting/routers/meeting.router.ts
@@ -1,13 +1,16 @@
 import { MeetingUserController } from '@modules/users/user';
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
+import { MeetingsController } from '../controllers';
 
 const meetingUserController = new MeetingUserController();
+const meetingsMeetingController = new MeetingsController();
 
 export const meetingRouter = function (
   fastify: FastifyInstance,
   options: FastifyPluginOptions,
   done: (error?: Error) => void,
 ) {
+  fastify.get('/', meetingsMeetingController.list);
   fastify.post('/', meetingUserController.create);
 
   done();
