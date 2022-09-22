88bb2cfaa76910cf5fb16c92e6cd23fc22b8b97a
nashjunior <nashjuniorjj@gmail.com>
2022-09-22 20:22:17 -0300

    feat: :art: should register meeting entites on meeter datasource

--- src/database/sources/Meeter.ts
+++ src/database/sources/Meeter.ts
@@ -1,10 +1,14 @@
 import { environment } from '@config/enviroment';
 import { DataSource } from 'typeorm';
 import * as usersEntities from '@modules/users/user/entities';
+import * as meetingsEntities from '@modules/meetings/meeting/entities';
 
 export const MeeterDataSource = new DataSource({
   type: 'postgres',
   url: environment.parsed?.DATABASE_URL,
   migrations: [environment.parsed?.PATH_MIGRATIONS as string],
-  entities: [...Object.values(usersEntities)],
+  entities: [
+    ...Object.values(usersEntities),
+    ...Object.values(meetingsEntities),
+  ],
 });
